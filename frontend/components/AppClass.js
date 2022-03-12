142f2a11c3b8ca818abd3845936d662eee9b11c5
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2022-03-11 21:46:25 -0600

    not broken but not working

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,13 +1,47 @@
 import React from 'react'
+import axios from 'axios'
+
+const URL = 'http://localhost:9000/api/result'
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      steps: 0,
+      grid: [(1,1), (2, 1), (3, 1), (1, 2), {'B': (2, 2)}, (3, 2), (1, 3), (2, 3), (3, 3)],
+      email: '',
+      message: ''
+      }
+  }
+  increment = evt => {
+    this.setState((state) => ({
+      ...state,
+      steps: state.steps + 1
+    }))
+  }
+  reset = evt => {
+    this.setState((state) => ({
+      ...state,
+      steps: 0
+    }))
+  }
+  getResults = () => {
+    axios.get(URL)
+      .then(res => {
+        debugger
+      })
+      .catch(err => {
+        debugger
+      })
+  }
+      
   render() {
     const { className } = this.props
-    return (
+      return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates 2, 2</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -24,11 +58,11 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.increment} id="left">LEFT</button>
+          <button onClick={this.increment}id="up">UP</button>
+          <button onClick={this.increment}id="right">RIGHT</button>
+          <button onClick={this.increment}id="down">DOWN</button>
+          <button onClick={this.reset}id="reset">reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
